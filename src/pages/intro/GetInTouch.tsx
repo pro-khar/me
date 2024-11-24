c4cdc5dabdd4498db579858552abce6630aa5e10
pro-khar <prakharvermamanu.veed@gmail.com>
2024-11-24 12:03:07 +0530

    video optim, get in touch cto

--- src/pages/intro/GetInTouch.tsx
+++ src/pages/intro/GetInTouch.tsx
@@ -16,12 +16,24 @@ function GetInTouch() {
         </Button>
       </PopoverTrigger>
       <PopoverContent className="flex gap-2 p-2 bg-primary border-black border-[3px] w-full">
-        <Button size="icon" variant="secondary">
-          <MdEmail />
-        </Button>
-        <Button size="icon" variant="secondary">
-          <FaLinkedin />
-        </Button>
+        <a
+          href="mailto:prakharvermamanu.veed@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Button size="icon" variant="secondary">
+            <MdEmail />
+          </Button>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/pro-khar/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Button size="icon" variant="secondary">
+            <FaLinkedin />
+          </Button>
+        </a>
       </PopoverContent>
     </Popover>
   );
